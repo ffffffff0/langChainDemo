bdf0c3b4fa5f420688793d61199259593cc93fe9
wen_xu <wen_xu@trendmicro.com>
2025-06-12 15:35:12 +0800

    update

--- RAG.py
+++ RAG.py
@@ -8,10 +8,13 @@ from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langgraph.graph import START, StateGraph
 from typing_extensions import List, TypedDict
 from langchain_core.vectorstores import InMemoryVectorStore
+from typing import Literal
+from typing_extensions import Annotated
+
 
 # Load and chunk contents of the blog
 loader = WebBaseLoader(
-    web_paths=("https://lilianweng.github.io/posts/2023-06-23-agent/",),
+    web_paths=("https://ramsayleung.github.io/zh/post/2025/%E6%9D%82%E8%B0%88ai%E5%8F%96%E4%BB%A3%E7%A8%8B%E5%BA%8F%E5%91%98/",),
     bs_kwargs=dict(
         parse_only=bs4.SoupStrainer(
             class_=("post-content", "post-title", "post-header")
@@ -23,6 +26,20 @@ docs = loader.load()
 text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
 all_splits = text_splitter.split_documents(docs)
 
+total_documents = len(all_splits)
+third = total_documents // 3
+
+for i, document in enumerate(all_splits):
+    if i < third:
+        document.metadata["section"] = "beginning"
+    elif i < 2 * third:
+        document.metadata["section"] = "middle"
+    else:
+        document.metadata["section"] = "end"
+
+
+print(f"all_split[0] metadata: {all_splits[0].metadata}")
+
 embeddings = openai_embeddings()
 vector_store = InMemoryVectorStore(embeddings)
 # Index chunks
@@ -33,15 +50,34 @@ _ = vector_store.add_documents(documents=all_splits)
 # api_url="https://api.smith.langchain.com" in hub.pull.
 prompt = hub.pull("rlm/rag-prompt")
 llm = openai_model()
+
+class Search(TypedDict):
+    """Search query."""
+    query: Annotated[str, ..., "Search query to run."]
+    section: Annotated[
+        Literal["beginning", "middle", "end"],
+        ...,
+        "Section to query.",
+    ]
 # Define state for application
 class State(TypedDict):
     question: str
+    query: Search
     context: List[Document]
     answer: str
 
+def analyze_query(state: State):
+    structured_llm = llm.with_structured_output(Search)
+    query = structured_llm.invoke(state["question"])
+    return {"query": query}
+
 # Define application steps
 def retrieve(state: State):
-    retrieved_docs = vector_store.similarity_search(state["question"])
+    query = state["query"]
+    retrieved_docs = vector_store.similarity_search(
+        query["query"],
+        filter=lambda doc: doc.metadata.get("section") == query["section"],
+    )
     return {"context": retrieved_docs}
 
 def generate(state: State):
@@ -51,23 +87,12 @@ def generate(state: State):
     return {"answer": response.content}
 
 # Compile application and test
-graph_builder = StateGraph(State).add_sequence([retrieve, generate])
-graph_builder.add_edge(START, "retrieve")
+graph_builder = StateGraph(State).add_sequence([analyze_query, retrieve, generate])
+graph_builder.add_edge(START, "analyze_query")
 graph = graph_builder.compile()
 
-result = graph.invoke({"question": "What is Task Decomposition?"})
-print(f'Context: {result["context"]}\n\n')
-print(f'Answer: {result["answer"]}')
-
-print(f"\n\n----------------\n")
-
 for step in graph.stream(
-    {"question": "What is Task Decomposition?"}, stream_mode="updates"
+    {"question": "AI 能否取代程序员?"}, stream_mode="updates"
 ):
     print(f"{step}\n\n----------------\n")
-
-for message, metadata in graph.stream(
-    {"question": "What is Task Decomposition?"}, stream_mode="messages"
-):
-    print(message.content, end="|")
 
