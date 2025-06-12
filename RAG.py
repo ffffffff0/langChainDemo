9d6fdb256c16699c1f65ce6f7b7a161ace370b35
wen_xu <wen_xu@trendmicro.com>
2025-06-12 16:26:45 +0800

    rag memory

--- RAG.py
+++ RAG.py
@@ -1,16 +1,17 @@
-from init_tool import openai_model, openai_embeddings
-# load page content
 import bs4
+from init_tool import openai_model, openai_embeddings
 from langchain import hub
 from langchain_community.document_loaders import WebBaseLoader
-from langchain_core.documents import Document
 from langchain_text_splitters import RecursiveCharacterTextSplitter
-from langgraph.graph import START, StateGraph
-from typing_extensions import List, TypedDict
+from langgraph.graph import StateGraph
 from langchain_core.vectorstores import InMemoryVectorStore
-from typing import Literal
-from typing_extensions import Annotated
-
+from langchain_core.tools import tool
+from langchain_core.messages import SystemMessage
+from langgraph.prebuilt import ToolNode
+from langgraph.graph import MessagesState, StateGraph
+from langgraph.graph import END
+from langgraph.prebuilt import ToolNode, tools_condition
+from langgraph.checkpoint.memory import MemorySaver
 
 # Load and chunk contents of the blog
 loader = WebBaseLoader(
@@ -23,7 +24,8 @@ loader = WebBaseLoader(
 )
 docs = loader.load()
 
-text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+text_splitter = RecursiveCharacterTextSplitter(
+    chunk_size=1000, chunk_overlap=200)
 all_splits = text_splitter.split_documents(docs)
 
 total_documents = len(all_splits)
@@ -51,48 +53,97 @@ _ = vector_store.add_documents(documents=all_splits)
 prompt = hub.pull("rlm/rag-prompt")
 llm = openai_model()
 
-class Search(TypedDict):
-    """Search query."""
-    query: Annotated[str, ..., "Search query to run."]
-    section: Annotated[
-        Literal["beginning", "middle", "end"],
-        ...,
-        "Section to query.",
-    ]
-# Define state for application
-class State(TypedDict):
-    question: str
-    query: Search
-    context: List[Document]
-    answer: str
 
-def analyze_query(state: State):
-    structured_llm = llm.with_structured_output(Search)
-    query = structured_llm.invoke(state["question"])
-    return {"query": query}
-
-# Define application steps
-def retrieve(state: State):
-    query = state["query"]
-    retrieved_docs = vector_store.similarity_search(
-        query["query"],
-        filter=lambda doc: doc.metadata.get("section") == query["section"],
+@tool(response_format="content_and_artifact")
+def retrieve(query: str):
+    """Retrieve information related to a query."""
+    retrieved_docs = vector_store.similarity_search(query, k=2)
+    serialized = "\n\n".join(
+        (f"Source: {doc.metadata}\n" f"Content: {doc.page_content}")
+        for doc in retrieved_docs
     )
-    return {"context": retrieved_docs}
+    return serialized, retrieved_docs
 
-def generate(state: State):
-    docs_content = "\n\n".join(doc.page_content for doc in state["context"])
-    messages = prompt.invoke({"question": state["question"], "context": docs_content})
-    response = llm.invoke(messages)
-    return {"answer": response.content}
+# Step 1: Generate an AIMessage that may include a tool-call to be sent.
 
-# Compile application and test
-graph_builder = StateGraph(State).add_sequence([analyze_query, retrieve, generate])
-graph_builder.add_edge(START, "analyze_query")
-graph = graph_builder.compile()
 
-for step in graph.stream(
-    {"question": "AI 能否取代程序员?"}, stream_mode="updates"
-):
-    print(f"{step}\n\n----------------\n")
+def query_or_respond(state: MessagesState):
+    """Generate tool call for retrieval or respond."""
+    llm_with_tools = llm.bind_tools([retrieve])
+    response = llm_with_tools.invoke(state["messages"])
+    # MessagesState appends messages to state instead of overwriting
+    return {"messages": [response]}
 
+
+# Step 2: Execute the retrieval.
+tools = ToolNode([retrieve])
+
+# Step 3: Generate a response using the retrieved content.
+
+
+def generate(state: MessagesState):
+    """Generate answer."""
+    # Get generated ToolMessages
+    recent_tool_messages = []
+    for message in reversed(state["messages"]):
+        if message.type == "tool":
+            recent_tool_messages.append(message)
+        else:
+            break
+    tool_messages = recent_tool_messages[::-1]
+
+    # Format into prompt
+    docs_content = "\n\n".join(doc.content for doc in tool_messages)
+    system_message_content = (
+        "You are an assistant for question-answering tasks. "
+        "Use the following pieces of retrieved context to answer "
+        "the question. If you don't know the answer, say that you "
+        "don't know. Use three sentences maximum and keep the "
+        "answer concise."
+        "\n\n"
+        f"{docs_content}"
+    )
+    conversation_messages = [
+        message
+        for message in state["messages"]
+        if message.type in ("human", "system")
+        or (message.type == "ai" and not message.tool_calls)
+    ]
+    prompt = [SystemMessage(system_message_content)] + conversation_messages
+
+    # Run
+    response = llm.invoke(prompt)
+    return {"messages": [response]}
+
+
+graph_builder = StateGraph(MessagesState)
+
+graph_builder.add_node(query_or_respond)
+graph_builder.add_node(tools)
+graph_builder.add_node(generate)
+
+graph_builder.set_entry_point("query_or_respond")
+graph_builder.add_conditional_edges(
+    "query_or_respond",
+    tools_condition,
+    {END: END, "tools": "tools"},
+)
+graph_builder.add_edge("tools", "generate")
+graph_builder.add_edge("generate", END)
+
+memory = MemorySaver()
+graph = graph_builder.compile(checkpointer=memory)
+# Specify an ID for the thread
+config = {"configurable": {"thread_id": "abc123"}}
+
+input_messages = [
+    "AI 和程序员的关系是怎样的?",
+    "这两个有什么共同特点和区别?"
+]
+for im in input_messages:
+    for step in graph.stream(
+        {"messages": [{"role": "user", "content": im}]},
+        stream_mode="values",
+        config=config
+    ):
+        step["messages"][-1].pretty_print()
